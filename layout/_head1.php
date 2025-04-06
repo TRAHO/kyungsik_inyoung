f319cafd8580a669bce01a30ed4eb2d67fb67e88
TRAHO <traho9750@naver.com>
2025-04-07 01:30:04 +0900

    og

--- layout/_head1.php
+++ layout/_head1.php
@@ -8,9 +8,10 @@
     <meta name="format-detection" content="telephone=no, address=no, email=no" />
 
     <meta property="og:type" content="website">
-    <meta property="og:title" content="경식&인영, 결혼합니다.">
-    <meta property="og:site_name" content="경식&인영, 결혼합니다.">
-    <meta property="og:description" content="경식&인영, 결혼합니다.">
-    <meta name="description" content="경식&인영, 결혼합니다.">
-    <!-- <link rel="icon" type="image/png" href="/asset/img/mobiem_favicon.png"> -->
+    <meta property="og:title" content="경식 ♥ 인영, 결혼합니다.">
+    <meta property="og:site_name" content="경식 ♥ 인영, 결혼합니다.">
+    <meta property="og:description" content="경식 ♥ 인영, 결혼합니다.">
+    <meta name="description" content="경식 ♥ 인영, 결혼합니다.">
+    <meta property="og:image" content="/asset/og.jpg">
+    <link rel="icon" type="image/png" href="/asset/favicon.png">
 </head>
